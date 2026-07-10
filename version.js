eb2eee086e3e1194b2abf265ddb4cfe30662f4b2
kucha-5 <yuyangchen2014@gmail.com>
2026-07-09 18:19:02 -0700

    Create version.js

---
+++ version.js
@@ -0,0 +1,20 @@
+{
+  "gameName": "Project Zero",
+  "latestVersion": "49.18.6",
+  "launcherVersion": "1.0.0",
+  "minimumLauncherVersion": "1.0.0",
+
+  "releaseDate": "2026-07-09",
+
+  "downloadUrl": "https://github.com/kucha-5/Project-Zero-releases/releases/download/49.18.6/ProjectZero_V49.18.6.zip",
+
+  "updateSize": "11.2 MB",
+  "forceUpdate": false,
+
+  "sha256": "",
+
+  "updateLog": [
+    "Project Zero 全新版本。",
+    "修复已知问题。"
+  ]
+}
